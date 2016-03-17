38ce14a533316f2df112ba49f98ced96f56d5519
belphegor666 <the_luggage@bigfoot.com>
2016-03-17 10:19:55 +0000

    Added VS source

---
+++ src/windows/ReverseRuntimeComponent/ReverseRuntimeComponent/ReversePluginRT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseRuntimeComponent
+{
+    public sealed class ReversePluginRT
+    {
+        public static String Reverse(String inStr)
+        {
+            String outStr = "";
+            for (int i=inStr.Length; i>0;i--)
+            {
+                outStr += inStr.ElementAt(i - 1);
+            }
+            return outStr;
+        }
+    }
+}
